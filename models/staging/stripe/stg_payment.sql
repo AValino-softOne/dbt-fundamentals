9db187a25ec40f8a670d1b8b68340110d314421c
AValino <arturo.valino@softwareone.com>
2022-03-28 10:44:01 +0000

    [st]_add_generic_test

--- models/staging/stripe/stg_payment.sql
+++ models/staging/stripe/stg_payment.sql
@@ -2,7 +2,7 @@ with payment as(
     select orderid as order_id,
     amount/100 as amount, --pasar de centimos a dolares
     status as status,
-     from `ip-trabajo-avalino.dbt_avalino.payment`
+     from {{ source('stripe', 'payment') }}
      where status='success'
 )
 

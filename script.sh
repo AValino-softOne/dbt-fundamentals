34afe32d9b8cf1417cbe03faf07106475c3b071b
AValino-softOne <arturo.valino@softwareone.com>
2022-04-01 11:52:07 +0000

    simplify

--- script.sh
+++ script.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-dbt deps --profiles-dir . # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt run --target prod --profiles-dir .
-dbt test --data --target dev --profiles-dir .
